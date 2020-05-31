edb87b0d7b258fbfddee56d24aae42f4d16d944e
Gkfaker <65300914+Gkfaker@users.noreply.github.com>
2020-05-31 19:59:48 +0300

    Student and StudentTeam classes ready

--- Stappler/src/_ObjectAndClasses/StudentTeam.java
+++ Stappler/src/_ObjectAndClasses/StudentTeam.java
@@ -1,9 +1,10 @@
 package _ObjectAndClasses;
 
-public class StudentTeam {
+public class StudentTeam extends Student {
 	private String name,team_desc;
 	
-	public StudentTeam() {
-		
+	public StudentTeam(String name, String description) {		//team constructor
+		this.name = name;
+		this.team_desc = description;
 	}
 }
