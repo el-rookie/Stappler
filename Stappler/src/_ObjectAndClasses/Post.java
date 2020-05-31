a66ca2da6dfaba752138a8967a8c2f8f4c8c3a50
John Stathopoulos <56523116+Sfixterman23@users.noreply.github.com>
2020-05-31 18:06:44 +0300

    Final src

--- Stappler/src/_ObjectAndClasses/Post.java
+++ Stappler/src/_ObjectAndClasses/Post.java
@@ -2,16 +2,19 @@ package _ObjectAndClasses;
 
 import java.util.Date;
 
-public class Post {
+public class Post extends Student {
 	private String title,post_content;
 	private int post_id;
 	private Date post_date;
 	
-	public Post() {
-		
+	public Post(String title, String content, int id, Date date) {  					//Post constructor
+		this.title = title;
+		this.post_content = content;
+		this.post_id = id;
+		this.post_date = new Date(System.currentTimeMillis());
 	}
 	
-	public String getAuthor() {
-		
+	public String getAuthor() {															//return the name of the author
+		return super.name;
 	}
 }
