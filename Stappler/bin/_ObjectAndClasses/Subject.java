b7ae1334bc652c00fd676ccf806ef922f767c75c
aakyrtsia <65234261+aakyrtsia@users.noreply.github.com>
2020-05-31 20:06:08 +0300

    Subject class ready

---
+++ Stappler/bin/_ObjectAndClasses/Subject.java
@@ -0,0 +1,17 @@
+package _ObjectAndClasses;
+
+import java.util.Date;
+
+public class Subject extends Student{
+	private String name, description;
+	private int id, semester;
+	private Date sub_date;
+	
+	public Subject() {
+		
+	}
+	
+	public void ECTS() {
+		
+	}
+}
